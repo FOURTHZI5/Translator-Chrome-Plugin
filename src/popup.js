831419265bdf8c64d043d470efaecdac299bb239
FOURTHZI5 <m18817952983@gmail.com>
2023-03-28 15:50:21 -0700

    Finish the tranlate function.

--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,16 @@
+const { Configuration, OpenAIApi } = require("openai");
+
 const inputText = document.getElementById('inputText');
 const translateButton = document.getElementById('translateButton');
 const outputText = document.getElementById('outputText');
 
 // 将YOUR_API_KEY替换为您的实际Chat GPT API密钥
-const apiKey = openAI_API_KEY;
-console.log('API Key:', apiKey);
+console.log('API Key:', openAI_API_KEY);
+
+const configuration = new Configuration({
+  apiKey: openAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 translateButton.addEventListener('click', async () => {
   const textToTranslate = inputText.value;
@@ -12,29 +18,24 @@ translateButton.addEventListener('click', async () => {
   outputText.value = translatedText;
 });
 
-async function translateText(text) {
-  const prompt = `Translate the following English text to Chinese: "${text}"`;
+async function translateText(text, language = 'Chinese') {
+  const prompt = `Translate this into ${language}:\n\n${text}\n\n1.`;
 
-  const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${apiKey}`
-    },
-    body: JSON.stringify({
-      prompt: prompt,
-      max_tokens: 100,
-      n: 1,
-      stop: null,
-      temperature: 0.5
-    })
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: prompt,
+    temperature: 0.3,
+    max_tokens: 100,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0
   });
 
-  const data = await response.json();
-  console.log(data)
+  const data = response.data;
+  console.log('Data:', data);
 
   if (data && data.choices && data.choices.length > 0) {
-    return data.choices[0].text.trim();
+    return data.choices[0].text;
   } else {
     return 'Translation failed. Please try again.';
   }
