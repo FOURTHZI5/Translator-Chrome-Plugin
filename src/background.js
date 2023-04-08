28b7d8ebcfa74c334f4635d2ba9eac80c87db8e0
FOURTHZI5 <m18817952983@gmail.com>
2023-04-08 15:15:57 -0700

    Finish the page fixed function.

--- src/background.js
+++ src/background.js
@@ -1,64 +1,38 @@
-const { Configuration, OpenAIApi } = require("openai");
-
-// 将YOUR_API_KEY替换为您的实际Chat GPT API密钥
-console.log("API Key:", openAI_API_KEY);
-
-const configuration = new Configuration({
-  apiKey: openAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
 chrome.contextMenus.create({
   id: "selectedText",
-  title: "Translate '%s' to Chinese",
+  title: "中文 '%s'",
   contexts: ["selection"],
 });
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   if (info.menuItemId === "selectedText") {
-    // chrome.scripting.executeScript({
-    //   target: {tabId: tab.id},
-    //   func: () => {
-    //     const selection = window.getSelection();
-    //     const range = selection.getRangeAt(0);
-    //     range.deleteContents();
-    //     range.insertNode(document.createTextNode("Hello World"));
-    //     selection.removeAllRanges();
-    //     selection.addRange(range);
-    //   }
-    // });
+    const response = await sendMessagePromise(tab.id, {
+      type: "translate-button-clicked",
+      text: info.selectionText,
+    });
+    console.log("Received translatedText:", response.translatedText);
+    const translatedText = response.translatedText;
 
-    const selectedText = info.selectionText;
-    const translatedText = await translateText(selectedText);
+    if (translatedText !== undefined) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: (translatedText) => {
+          const selection = window.getSelection();
+          const range = selection.getRangeAt(0);
+          range.deleteContents();
+          range.insertNode(document.createTextNode(translatedText));
+          console.log("Injected translatedText", translatedText);
+          selection.removeAllRanges();
+          selection.addRange(range);
+        },
+        args: [translatedText],
+      });
+    }
   }
 });
 
-async function translateText(text, language = "Chinese") {
-  const prompt = `Translate this into ${language}:\n\n${text}\n\n`;
-
-  console.log('prompt', prompt)
-
-  try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      temperature: 0.3,
-      max_tokens: 100,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
-
-
-    const data = response.data;
-    console.log("Data:", data);
-
-    if (data && data.choices && data.choices.length > 0) {
-      return data.choices[0].text;
-    } else {
-      return "Translation failed. Please try again.";
-    }
-  } catch (error) {
-    console.log("Failed to query the openAI, find error:", error);
-  }
+function sendMessagePromise(tabId, message) {
+  return new Promise((resolve) => {
+    chrome.tabs.sendMessage(tabId, message, resolve);
+  });
 }
--- src/content.js
+++ src/content.js
@@ -1,27 +1,54 @@
-// console.log('Content script is running');
-// const div = document.createElement('div');
-// div.innerHTML = 'Hello from content script!';
-// document.body.appendChild(div);
+const { Configuration, OpenAIApi } = require("openai");
 
-function getSelectionText() {
-  var text = "";
-  var activeEl = document.activeElement;
-  var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
-  if (
-    (activeElTagName == "textarea") || (activeElTagName == "input" &&
-    /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
-    (typeof activeEl.selectionStart == "number")
-  ) {
-    text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
-  } else if (window.getSelection) {
-    text = window.getSelection().toString();
-  }
-  return text;
+// 将YOUR_API_KEY替换为您的实际Chat GPT API密钥
+console.log("API Key:", openAI_API_KEY);
+
+const configuration = new Configuration({
+  apiKey: openAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+console.log("Openai config:", openai);
+async function runTranslation() {
+  const first_translatedText = await translateText("Hello world");
+  console.log("First translation:", first_translatedText);
 }
 
-// document.addEventListener("mouseup", function(event) {
-//   var selectionText = getSelectionText();
-//   if (selectionText) {
-//     chrome.runtime.sendMessage({ selectionText: selectionText });
-//   }
-// });
+runTranslation();
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "translate-button-clicked") {
+    translateText(request.text).then((translatedText) => {
+      console.log("Translated text:", translatedText);
+      sendResponse({ translatedText: translatedText });
+    });
+  }
+  return true;
+});
+
+async function translateText(text, language = "Chinese") {
+  const prompt = `Translate this into ${language}:\n\n${text}\n\n`;
+
+  console.log("prompt", prompt);
+
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.3,
+      max_tokens: 1000,
+    });
+
+    const data = response.data;
+    console.log("Data:", data);
+
+    if (data && data.choices && data.choices.length > 0) {
+      console.log("Translated text:", data.choices[0].text);
+      return data.choices[0].text;
+    } else {
+      return "Translation failed. Please try again.";
+    }
+  } catch (error) {
+    console.log("Failed to query the openAI, find error:", error);
+  }
+}
