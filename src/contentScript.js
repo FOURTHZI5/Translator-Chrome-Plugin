c9d21ab4d13d8d6fd3ba08214b06de9a903e7eba
FOURTHZI5 <m18817952983@gmail.com>
2023-04-23 01:29:27 -0700

    failed to add the stream

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,33 +1,30 @@
 const { Configuration, OpenAIApi } = require("openai");
 const { createPopper } = require("@popperjs/core");
 
-// 将YOUR_API_KEY替换为您的实际Chat GPT API密钥
-// console.log("API Key:", openAI_API_KEY);
-
 const configuration = new Configuration({
   apiKey: openAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-// console.log("Openai config:", openai);
-// async function runTranslation() {
-//   const first_translatedText = await translateText("Hello world");
-//   console.log("First translation:", first_translatedText);
-// }
-
-// runTranslation();
-
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.type === "translate-button-clicked") {
-    // console.log("Popper script executed");
-
     try {
-      const translatedText = await translateText(request.text);
-      // console.log("Translated text:", translatedText);
+      const streamResponse = await openai.createCompletion(
+        {
+          model: "text-davinci-003",
+          prompt: `Translate the following text into Chinese, keep the format:\n---------------------------\n\n${request.text}`,
+          temperature: 0.3,
+          max_tokens: 1000,
+          stream: true,
+        },
+        { responseType: "stream" }
+      );
+      // console.log("stream data", streamResponse.data);
+
       const range = window.getSelection().getRangeAt(0);
       const referenceElement = range.startContainer.parentElement;
+
       const tooltip = document.createElement("div");
-      tooltip.textContent = translatedText;
       tooltip.className = "tooltip";
 
       const arrow = document.createElement("div");
@@ -36,31 +33,56 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
 
       document.body.appendChild(tooltip);
 
-      document.addEventListener("click", function(event) {
+      document.addEventListener("click", function (event) {
         if (tooltip.parentNode && !tooltip.contains(event.target)) {
           tooltip.parentNode.removeChild(tooltip);
         }
       });
 
-      createPopper(referenceElement, tooltip, {
-        placement: "bottom",
-        modifiers: [
-          {
-            name: "offset",
-            options: {
-              offset: ({ placement, reference, popper }) => {
-                if (placement === 'bottom') {
-                  return [0, popper.height / 2];
-                } else {
-                  return [];
-                }
-              },
-            },
-          },
-        ],
-      });
+      let data = "";
+      for await (const chunk of streamResponse) {
+        data += chunk;
+        console.log(chunk);
+        // Handle the received data here
+      }
+      console.log("data", data);
 
-      sendResponse("Translation completed");
+      // stream.data.on("data", (data) => {
+      //   const response = JSON.parse(data.toString());
+      //   const translatedText = response.choies[0].text;
+      //   console.log(translatedText);
+
+      //   tooltip.textContent = translatedText;
+      // });
+
+      // stream.data.on("error", (error) => {
+      //   console.log("Failed to translate the text:", error);
+      //   sendResponse({ error: error.message });
+      // });
+
+      // stream.data.on("end", () => {
+      //   console.log("Translation completed");
+      //   sendResponse("Translation completed");
+      // });
+
+      // createPopper(referenceElement, tooltip, {
+      //   placement: "bottom",
+      //   modifiers: [
+      //     {
+      //       name: "offset",
+      //       options: {
+      //         offset: ({ placement, reference, popper }) => {
+      //           if (placement === 'bottom') {
+      //             return [0, popper.height / 2];
+      //           } else {
+      //             return [];
+      //           }
+      //         },
+      //       },
+      //     },
+      //   ],
+      // });
+      console.log("Translation completed");
     } catch (error) {
       console.log("Failed to translate the text:", error);
       sendResponse({ error: error.message });
@@ -70,31 +92,20 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   }
 });
 
-async function translateText(text, language = "Chinese") {
+async function createTranslationStream(text, language = "Chinese") {
   const prompt = `Translate the following text into ${language}, keep the format:\n---------------------------\n\n${text}`;
 
-  // console.log("prompt", prompt);
-
   try {
-    const response = await openai.createCompletion({
+    const stream = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: prompt,
       temperature: 0.3,
       max_tokens: 1000,
+      stream: true,
     });
-
-    const data = response.data;
-    // console.log("Data:", data);
-
-    if (data && data.choices && data.choices.length > 0) {
-      const translatedText = data.choices[0].text;
-      // console.log("Translated text:", translatedText);
-      return translatedText;
-    } else {
-      return "Translation failed. Please try again.";
-    }
+    return stream;
   } catch (error) {
-    console.log("Failed to query the OpenAI API:", error);
+    console.log("Failed to create the stream:", error);
     throw error;
   }
 }
